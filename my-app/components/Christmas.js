a69d12da520b3e5bc60cc2d92f41337e5562db25
Siiri Saar <siiri.saar@easypark.net>
2021-12-07 16:06:47 +0200

    First commit

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
-            <h2>Is it christmas yet? {christmas.isItChristmasYet ? 'Yep' : 'Nope'}</h2>
+            <div className="container">
+                <ReactPlayer url='macarena.mp4' playing={true} loop={true} width='100%' height='100%' />
+            </div>
         </div>
     )
 }
